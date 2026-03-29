2fe805acf159f0b1f2d65acc947be6f5ae8c13d2
Emobies & TheWall = Emowall 2.0 <248824152+Emobies05@users.noreply.github.com>
2026-03-29 08:05:26 +0400

    Add handleGithub function to github.js

--- worker/routes/github.js
+++ worker/routes/github.js
@@ -1,1 +1,6 @@
-
+export async function handleGithub(request, env) {
+  return new Response(
+    JSON.stringify({ route: "github", status: "ok" }),
+    { status: 200, headers: { "Content-Type": "application/json" } }
+  )
+}
