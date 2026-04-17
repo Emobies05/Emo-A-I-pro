8c47a1275b19c7d5d00b64c43a7df652c2a11a80
THEWIN <248824152+Emobies05@users.noreply.github.com>
2026-04-17 06:56:03 +0400

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'controllers/butterfly_controller.dart';
+import 'screens/chat_screen.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ButterflyState(),
+      child: const EmoAIProApp(),
+    ),
+  );
+}
+
+class EmoAIProApp extends StatelessWidget {
+  const EmoAIProApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Emo AI Pro',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
+      home: const ChatScreen(),
+    );
+  }
+}
