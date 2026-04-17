db0a24356220e54d5d7a4e086381283819e16983
THEWIN <248824152+Emobies05@users.noreply.github.com>
2026-04-17 06:52:52 +0400

    Update thewall_flower.dart

--- lib/widgets/thewall_flower.dart
+++ lib/widgets/thewall_flower.dart
@@ -3,27 +3,34 @@ import 'package:flutter/material.dart';
 class TheWallFlower extends StatelessWidget {
   final Offset position;
 
-  const TheWallFlower({super.key, required this.position});
+  const TheWallFlower({
+    super.key,
+    required this.position,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
       left: position.dx,
       top: position.dy,
-      child: Container(
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.pinkAccent.withOpacity(0.4),
-              blurRadius: 60,
-              spreadRadius: 10,
-            ),
-          ],
-        ),
-        child: Image.asset(
-          'assets/thewall_flower.png',
-          width: 120,
-          height: 120,
+      child: IgnorePointer(
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.pinkAccent.withOpacity(0.45),
+                blurRadius: 60,
+                spreadRadius: 12,
+              ),
+            ],
+          ),
+          child: Image.asset(
+            'assets/thewall_flower.png',
+            width: 120,
+            height: 120,
+            fit: BoxFit.contain,
+          ),
         ),
       ),
     );
