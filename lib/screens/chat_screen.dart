5c0b648cb35aaa4afa15f18f15cac22a571086a1
THEWIN <248824152+Emobies05@users.noreply.github.com>
2026-04-18 04:47:29 +0400

    Update chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,9 +5,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:flutter_tts/flutter_tts.dart';
 
-import 'emo_api.dart';
-import 'controllers/butterfly_controller.dart';
-import 'widgets/thewall_butterfly.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:flutter_tts/flutter_tts.dart';
+import '../widgets/emo_butterfly.dart';
+
+class ChatScreen extends StatefulWidget {
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
